1e424d9f0e4645d40517fb365416a510fc808949
Abbas khan <145669575+sp23-bse-115@users.noreply.github.com>
2025-05-03 11:48:37 +0500

    Create SystemData.java

---
+++ SystemData.java
@@ -0,0 +1,11 @@
+class SystemData {
+    private Map<String, Object> data;
+    
+    public SystemData(Map<String, Object> data) {
+        this.data = data;
+    }
+    
+    public Map<String, Object> getData() {
+        return data;
+    }
+}
